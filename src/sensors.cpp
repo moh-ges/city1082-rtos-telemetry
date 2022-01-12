3ced285d4db6f46e0d1adadb9c2bb6f5c4f2ac90
asreed666 <areed@cityplym.ac.uk>
2022-01-12 09:57:43 +0000

    coeff update

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -6,17 +6,20 @@
 #define THERM_VCC P10_3
 #define THERM_OUT P10_1
 
+
+#define THERM_GND P10_3
+#define THERM_VCC P10_0
+#define THERM_OUT P10_1
+
 /* Reference resistor in series with the thermistor is of 10kohm */
 #define R_REFERENCE                         (float)(10000)
 
-/* Beta constant of NCP18XH103F03RB thermistor is 3380 Kelvin. See the thermistor
-   data sheet for more details. */
-#define B_CONSTANT                          (float)(3380)
-
-/* Resistance of the thermistor is 10K at 25 degrees C (from the data sheet)
-   Therefore R0 = 10000 Ohm, and T0 = 298.15 Kelvin, which gives
-   R_INFINITY = R0 e^(-B_CONSTANT / T0) = 0.1192855 */
-#define R_INFINITY                          (float)(0.1192855)
+/* A constant of NCP18XH103F03RB thermistor is */
+#define A_COEFF                          (float)(0.0009032679f)
+/* B constant of NCP18XH103F03RB thermistor is */
+#define B_COEFF                          (float)(0.000248772f)
+/* C constant of NCP18XH103F03RB thermistor is */
+#define C_COEFF                          (float)(0.0000002041094f)
 
 /* Zero Kelvin in degree C */
 #define ABSOLUTE_ZERO                       (float)(-273.15)
@@ -41,15 +44,8 @@ void sendThread(void)
     uint32_t i = 0;
     while (true) {
         i++; // fake data update
-/*        temp = tempVoltage.read() * 3.3f; */
         float temperature;
-/*        float rThermistor; */
 
-        /* Calculate the thermistor resistance */
-//        rThermistor = tempVoltage.read() * R_REFERENCE;
-
-        /* Calculate the temperature in deg C */
-//        temperature = (B_CONSTANT/(logf(rThermistor/R_INFINITY))) + ABSOLUTE_ZERO;
         temperature = readTemp();
         lightLev = fmod((i * 0.1f) * 5.5f, 100);
         cycles = i;
@@ -62,13 +58,13 @@ void sendThread(void)
 float readTemp()
 {
     float refVoltage = tempVoltage.read() * 2.4; // Range of ADC 0->2*Vref
-    float refCurrent = refVoltage  / 10000.0; // 10k Reference Resistor
+    float refCurrent = refVoltage  / R_REFERENCE; // 10k Reference Resistor
     float thermVoltage = 3.3 - refVoltage;    // Assume supply voltage is 3.3v
     float thermResistance = thermVoltage / refCurrent; 
     float logrT = (float32_t)log((float64_t)thermResistance);
     /* Calculate temperature from the resistance of thermistor using Steinhart-Hart Equation */
-    float stEqn = (float32_t)((0.0009032679) + ((0.000248772) * logrT) + 
-                             ((2.041094E-07) * pow((float64)logrT, (float32)3)));
-    float temperatureC = (float32_t)(((1.0 / stEqn) - 273.15)  + 0.5);
+    float stEqn = (float32_t)((A_COEFF) + ((B_COEFF) * logrT) + 
+                             ((C_COEFF) * pow((float64)logrT, (float32)3)));
+    float temperatureC = (float32_t)(((1.0 / stEqn) + ABSOLUTE_ZERO)  + 0.05);
     return temperatureC;
 }
